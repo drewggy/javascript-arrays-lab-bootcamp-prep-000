90506b7fb5b361d83ec8a4487dcc9b54f0e686c9
Andrew Sommer <sommer.drew@gmail.com>
2018-06-08 13:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+    windows.kitten.push(name);
+    return;
+}
+function destructivelyPrependKitten(name) {
+
+}
+function destructivelyRemoveLastKitten(){
+
+}
+function destructivelyRemoveFirstKitten(){
+
+}
+function appendKitten(name){
+
+}
+function prependKitten(name) {
+
+}
+function removeLastKitten(){
+
+}
+functon removeFirstKitten(){
+
+}
